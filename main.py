a58017b534616e65af11ab619c4a4a246c5ccaea
double-coder <getaddi1711@gmail.com>
2023-02-03 20:40:09 +0530

    add comments

--- main.py
+++ main.py
@@ -5,16 +5,32 @@ from stringops import flipbit
 from linkedlist import createalinkedlist
 
 def main():
+    # binary search
     sample = [-4,-1,0,3,10]
     sol = finditem.Solution
     x = sol.search(sol, [-1,0,3,5,9,12], 9)
     print(x)
+    
+    # easy two pointers question
     sortarr = arraychan.sortedSquares
     print(sortarr(sortarr, sample))
+    
+    """
+    medium question from leet code
+    Q. when you see # hit backspace and compare if both strings are same?
+    """
     com = comparearr.Solution
     com.backspaceCompare(com, 'ab##', 'c#d#')
+    """
+    medium level question from leat code
+    Q. how man bits we need to flip to get to target?
+       target = 7
+       source = 10
+    """
     bitops = flipbit.Solution
     bitops.minBitFlips(bitops, 10, 7)
+
+    # linked list implementation
     ll = createalinkedlist.LinkedList()
     Node = createalinkedlist.Node
     ll.head = Node(1)
